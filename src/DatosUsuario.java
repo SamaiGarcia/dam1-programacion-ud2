0129aa850f1189dbd9ae8bae0e9319e687f15ce8
SamaiGarcia <samaigarcia7@gmail.com>
2025-10-02 09:17:52 +0200

    yes

---
+++ src/DatosUsuario.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import utilidades.ProcesadorTexto;
+import utilidades.Usuario;
+
+
+public class DatosUsuario {
+
+
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in );
+
+ System.out.println("Introduce el nombre:");
+ String nombre = sc.nextLine();
+
+ System.out.println("Introduce tu edad:");
+ int edad = sc.nextInt();
+
+System.out.println("Introduce tu correo electronico:");
+ String correo = sc.nextLine();
+
+boolean correovalido =ProcesadorTexto.esEmailValido(correo);
+boolean edadValida = edad < 0 || edad > 120; 
+
+if(correovalido && edadValida) {
+       new Usuario (nombre,edad,correo);
+    } else  {
+            System.out.println("El usuario no puede ser creado porque el email introducido no es válido.");
+        
+       
+String name = "samai";
+System.out.println("El nombre es:;"+ name.substring(0,1).toUpperCase()+ name.substring(1).toLowerCase());
+
+        sc.close();
+
+         }
+}
+}
+
+
+
